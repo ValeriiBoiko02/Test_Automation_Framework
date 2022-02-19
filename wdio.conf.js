d13be54de7136f477fd2831e96e9987a6eb4c506
Valerii <boikov01@kits.kfplc.com>
2022-02-20 00:57:59 +0200

    changes on standalone

--- wdio.conf.js
+++ wdio.conf.js
@@ -1,7 +1,7 @@
 exports.config = {
 
-    user: process.env.BROWSERSTACK_USERNAME,
-    key: process.env.BROWSERSTACK_KEY,
+  //  user: process.env.BROWSERSTACK_USERNAME,
+   // key: process.env.BROWSERSTACK_KEY,
     //
     // ====================
     // Runner Configuration
@@ -119,7 +119,8 @@ exports.config = {
     // Services take over a specific job you don't want to take care of. They enhance
     // your test setup with almost no effort. Unlike plugins, they don't add new
     // commands. Instead, they hook themselves up into the test process.
-    services: ['browserstack'],
+    //services: ['browserstack'],
+    services: ['selenium-standalone'],
 
     // Framework you want to run your specs with.
     // The following are supported: Mocha, Jasmine, and Cucumber
